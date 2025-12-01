ec02b852812daf045876c51591e0d0fa5069ef45
Towhid Ahmed <121598055+TowhidAhmedd@users.noreply.github.com>
2025-12-01 18:42:49 +0600

    Update face_mask_detection_using_convolutional_neural_network_(cnn).py

--- face_mask_detection_using_convolutional_neural_network_(cnn).py
+++ face_mask_detection_using_convolutional_neural_network_(cnn).py
@@ -1,3 +1,7 @@
+
+# Work Flow = Dataset -> Image processing -> Train Test Split -> CNN -> Evaluation
+
+# Dependency
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,12 +10,8 @@ import cv2
 from google.colab.patches import cv2_imshow
 from PIL import Image
 from sklearn.model_selection import train_test_split
-
-
 from google.colab import drive
 drive.mount('/content/drive')
-
-import os
 
 # MyDrive
 print(os.listdir("/content/drive/MyDrive"))
@@ -19,19 +19,14 @@ print(os.listdir("/content/drive/MyDrive"))
 with_mask= "/content/drive/MyDrive/with_mask"
 print(os.listdir(with_mask))  # folder
 
-
-import os
-print(os.listdir(with_mask))
-
 without_mask = "/content/drive/MyDrive/without_mask"
 print(os.listdir(without_mask))  # subfolders
 
 
-import os
-print(os.listdir(without_mask))
 
-print('Number of with mask images:', len(with_mask))
-print('Number of without mask images:', len(without_mask))
+
+print('Number of with mask images:', len(with_mask)) # length of mask dataset
+print('Number of without mask images:', len(without_mask)) # length of without mask dataset
 
 """Creating Labels for the two class of Images
 
@@ -43,11 +38,12 @@ without mask --> 0
 # create the label
 # Dynamically get the number of files from the lists created in the image processing step
 with_mask_files = os.listdir(with_mask)
-without_mask_files = os.listdir(without_mask)
+without_mask_files = os.listdir(withot_mask)
 
 num_with_mask_images = len(with_mask_files)
 num_without_mask_images = len(without_mask_files)
 
+# create the label for the two class of images
 with_mask_label = [1] * num_with_mask_images
 without_mask_label = [0] * num_without_mask_images
 
@@ -57,8 +53,9 @@ print(without_mask_label[0:5])
 print(len(with_mask_label))
 print(len(without_mask_label))
 
-labels = with_mask_label + without_mask_label
 
+# combine with_mask_label + without_mask_label
+labels = with_mask_label + without_mask_label
 print(len(labels))
 print(labels[0:5])
 print(labels[-5:])
@@ -66,13 +63,11 @@ print(labels[-5:])
 """Diaplaing the images"""
 
 # displaying with mask image
-import os
 img = mpimg.imread(os.path.join(with_mask, 'with_mask_347.jpg')) # Using an image from the listed files in 'with_mask'
 imgplot = plt.imshow(img)
 plt.show()
 
 # displaying without mask image
-import os
 img = mpimg.imread(os.path.join(without_mask, 'without_mask_3555.jpg')) # Using an image from the listed files in 'without_mask'
 imgplot = plt.imshow(img)
 plt.show()
@@ -88,32 +83,28 @@ plt.show()
 data = []
 
 with_mask_path = "/content/drive/MyDrive/with_mask/"
-
 for img_file in with_mask_files:
   image = Image.open(with_mask_path+img_file)
   image = image.resize((128,128))
-  data.append(np.array(image))
-  data.append(np.array(image))
-
+  image = image.convert('RGB')
+  image = np.array(image)
+  data.append(image)
 
 
 with_mask_path = "/content/drive/MyDrive/with_mask/"
-
 for img_file in with_mask_files:
   image = Image.open(with_mask_path+img_file)
   image = image.resize((128,128))
-  data.append(np.array(image))
-  data.append(np.array(image))
+  image = image.convert('RGB')
+  image = np.array(image)
+  data.append(image)
 
-len(data)
+len(data) # length of data
+type(data) # type of data
+data[0] # print first data
+type(data[0]) # first datatype
+data[0].shape # first datatype shape
 
-type(data)
-
-data[0]
-
-type(data[0])
-
-data[0].shape
 
 # convert images to numpy arrays
 data = []
@@ -169,14 +160,15 @@ for img_file in without_mask_files:
         except Exception as e:
             print(f"Error processing without_mask/{img_file}: {e}")
 
+
 # converting image list and lavel list to numpy arrays
 X = np.array(data)
 Y = np.array(labels)
 
 print(X.shape)
 print(Y.shape)
-
 print(Y)
+
 
 """Train Test Split"""
 
@@ -184,15 +176,15 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 
 print(X.shape, X_train.shape, X_test.shape,Y_train.shape, Y_test.shape)
 
+
 # scaling the data
 
 X_train_scaled = X_train/255
-
 X_test_scaled = X_test/255
 
 X_train[0]
-
 X_train_scaled[0]
+
 
 """Building a Convolutional Neural Networks (CNN)"""
 
@@ -203,29 +195,27 @@ from tensorflow import keras
 num_of_classes = 2
 
 model = keras.Sequential()
-
 model.add(keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(128,128,3)))
 model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
-
 
 model.add(keras.layers.Conv2D(64, kernel_size=(3,3), activation='relu'))
 model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
 
 model.add(keras.layers.Flatten())
-
 model.add(keras.layers.Dense(128, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
 
 model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
 
-
 model.add(keras.layers.Dense(num_of_classes, activation='sigmoid'))
+
 
 # compile the neural network
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['acc'])
+
 
 # training the neural network
 history = model.fit(X_train_scaled, Y_train, validation_split=0.1, epochs=5)
@@ -235,6 +225,7 @@ history = model.fit(X_train_scaled, Y_train, validation_split=0.1, epochs=5)
 loss, accuracy = model.evaluate(X_test_scaled, Y_test)
 print('Test Accuracy =', accuracy)
 
+# show line using matplotlib
 h = history
 
 # plot the loss value
